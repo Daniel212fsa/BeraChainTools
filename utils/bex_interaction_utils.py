c1a169a5d56a54956a44b2add6b1b6864c716183
kukuxz007 <kukuxz007@gmail.com>
2024-01-29 22:36:19 +0800

    兑换流程随机

--- utils/bex_interaction_utils.py
+++ utils/bex_interaction_utils.py
@@ -8,11 +8,7 @@ from config.address_config import (
 )
 
 
-def bex_interacte(private_key, rpc_url):
-    logger.debug('开始Bex 交互')
-
-    account = Account.from_key(private_key)
-    bera = BeraChainTools(private_key=account.key, rpc_url=rpc_url)
+def swap_usdc(account, bera):
     for i in range(10):
         try:
             # bex 使用bera交换usdc
@@ -25,7 +21,7 @@ def bex_interacte(private_key, rpc_url):
             random_amount = round(random.uniform(0.10, 0.20), 2)
             result = bera.bex_swap(int(bera_balance * random_amount), wbear_address, usdc_address)
             usdc_balance_new = bera.usdc_contract.functions.balanceOf(account.address).call()
-            print('usdc_balance',usdc_balance,'usdc_balance_new',usdc_balance_new)
+            print('usdc_balance', usdc_balance, 'usdc_balance_new', usdc_balance_new)
             if result:
                 logger.success(f'bex 使用bera交换usdc成功！！！,{result}')
                 break
@@ -34,6 +30,8 @@ def bex_interacte(private_key, rpc_url):
         except Exception as e:
             logger.error(f'bex 使用bera交换usdc失败！！！,{e}')
 
+
+def swap_weth(account, bera):
     for j in range(10):
         try:
             # bex 使用bera交换weth
@@ -55,6 +53,8 @@ def bex_interacte(private_key, rpc_url):
         except Exception as e:
             logger.error(f'bex 使用bera交换weth失败！！！,{e}')
 
+
+def add_liquidity_usdc(account, bera):
     for k in range(10):
         try:
             # 授权usdc
@@ -74,6 +74,8 @@ def bex_interacte(private_key, rpc_url):
         except Exception as e:
             logger.error(f'bex 增加 usdc 流动性失败！！！,{e}')
 
+
+def add_liquidity_weth(account, bera):
     for t in range(10):
         try:
             # 授权weth
@@ -92,6 +94,44 @@ def bex_interacte(private_key, rpc_url):
                 logger.error(f'bex 增加 weth 流动性失败！！！,{result}')
         except Exception as e:
             logger.error(f'bex 增加 weth 流动性失败！！！,{e}')
+
+
+def bex_interacte(private_key, rpc_url):
+    logger.debug('开始Bex 交互')
+
+    account = Account.from_key(private_key)
+    bera = BeraChainTools(private_key=account.key, rpc_url=rpc_url)
+    random_number = random.randint(1, 6)
+    if random_number == 1:
+        swap_usdc(account, bera)
+        swap_weth(account, bera)
+        add_liquidity_usdc(account, bera)
+        add_liquidity_weth(account, bera)
+    elif random_number == 2:
+        swap_usdc(account, bera)
+        swap_weth(account, bera)
+        add_liquidity_weth(account, bera)
+        add_liquidity_usdc(account, bera)
+    elif random_number == 3:
+        swap_weth(account, bera)
+        swap_usdc(account, bera)
+        add_liquidity_usdc(account, bera)
+        add_liquidity_weth(account, bera)
+    elif random_number == 4:
+        swap_weth(account, bera)
+        swap_usdc(account, bera)
+        add_liquidity_weth(account, bera)
+        add_liquidity_usdc(account, bera)
+    elif random_number == 5:
+        swap_usdc(account, bera)
+        add_liquidity_usdc(account, bera)
+        swap_weth(account, bera)
+        add_liquidity_weth(account, bera)
+    elif random_number == 6:
+        swap_weth(account, bera)
+        add_liquidity_weth(account, bera)
+        swap_usdc(account, bera)
+        add_liquidity_usdc(account, bera)
 
     logger.success('Bex 交互结束')
     logger.debug('-------------------------------------------------------------------------------------')
