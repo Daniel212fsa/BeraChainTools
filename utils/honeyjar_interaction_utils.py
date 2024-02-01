2ea7b55fd2493460cd1a7587b7be6f58a9339aba
kukuxz007 <kukuxz007@gmail.com>
2024-02-01 15:22:15 +0800

    更新部分提示

--- utils/honeyjar_interaction_utils.py
+++ utils/honeyjar_interaction_utils.py
@@ -4,7 +4,7 @@ from eth_account import Account
 from loguru import logger
 
 from bera_tools import BeraChainTools
-from config.address_config import ooga_booga_address, honey_address, wbear_address,usdc_address
+from config.address_config import ooga_booga_address, honey_address, wbear_address, usdc_address
 
 
 # from config.address_config import bex_swap_address, usdc_address, honey_address, honey_swap_address, \
@@ -29,23 +29,27 @@ def honeyjar_interacte_(private_key, rpc_url):
 
         honey_balance = bera.honey_contract.functions.balanceOf(account.address).call()
         approve_result = bera.approve_token(ooga_booga_address, 5 * 10 ** 18, honey_address)
-        if approve_result and honey_balance < 42 * 10 ** 17:
-            for i in range(10):
-                random_amount = round(random.uniform(4.30, 6.20), 2)
-                result = bera.bex_swap(int(random_amount * 10 ** 18), usdc_address, honey_address)
-                if result:
-                    break
-        else:
-            logger.success(f'授权失败或者交换失败')
+        if approve_result:
+            if honey_balance < 42 * 10 ** 17:
+                for i in range(10):
+                    random_amount = round(random.uniform(4.30, 6.20), 2)
+                    result = bera.bex_swap(int(random_amount * 10 ** 18), usdc_address, honey_address)
+                    if result:
+                        break
+            else:
+                logger.success(f'无须兑换,直接mint')
 
-        result = bera.honey_jar_mint()
-        if result:
-            logger.success(f'0xhoneyjar 交互成功,{result}')
-            logger.debug('-------------------------------------------------------------------------------------')
-            return True
+            result = bera.honey_jar_mint()
+            if result:
+                logger.success(f'0xhoneyjar 交互成功,{result}')
+                logger.debug('-------------------------------------------------------------------------------------')
+                return True
+            else:
+                return False
         else:
+            logger.error(f'授权失败!')
             return False
     except Exception as e:
         logger.error(f'0xhoneyjar 交互失败，{e}')
-        time.sleep(5)
+        # time.sleep(5)
     return False
