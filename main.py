f614aedcc8cb54e0e02f427dbdfce5ac07f6e3d9
daniel <daniel@qq.com>
2024-02-18 12:24:14 +0800

    新增两个NTF

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-import random
-import os
-import time
 import configparser
+import random
+import time
+
 from eth_account import Account
 from loguru import logger
 
+from bera_tools import BeraChainTools
 from utils.bend_interaction_utils import bend_interacte
 from utils.bex_interaction_utils import bex_interacte
 from utils.deploy_contract import deploy_contract
 from utils.honetjar_nft_mint import nft_mint
 from utils.honey_interaction_utils import honey_interacte
 from utils.honeyjar_interaction_utils import honeyjar_interacte
-from utils.waters_utils import generate_wallet
-from bera_tools import BeraChainTools
-from utils.waters_utils import get_proxy
-import concurrent.futures
-from functools import partial
 
 
 def only_claim(arg):
@@ -85,25 +81,7 @@ def claim_and_action(arg):
                               client_key=client_key,
                               solver_provider=solver_provider,
                               rpc_url=rpc_url)
-        has_mint = bera.nft_contract.functions.hasMinted(account.address).call()
-        if has_mint:
-            logger.debug(f"第{index}次交互,无需重复交互")
-            return
-        for i in range(10):
-            balance = bera.get_balance()
-            logger.debug(f"第{index}次交互,测试币余额 {balance / 10 ** 18}")
-            if balance < 4 * 10 ** 16:
-                try:
-                    result = bera.claim_bera(proxies=get_proxy(proxy_url))
-                    if 'Txhash' in result.text or 'to the queue' in result.text:
-                        logger.success(f'第{index}次交互,领水成功,{result.text}\n')
-                        break
-                    else:
-                        logger.error(f'第{index}次交互,领水失败,{result.text}\n')
-                except Exception as e:
-                    logger.error(f'第{index}次交互,领水失败,{e}\n')
-            else:
-                break
+
         balance = bera.get_balance()
         if balance > 0:
             bex_interacte(private_key, rpc_url, index)
@@ -126,42 +104,25 @@ def claim_and_action(arg):
 
 if __name__ == '__main__':
     # 是否为初始化钱包模式
-    mode_init_wallet = False
     config = configparser.ConfigParser()
-    config.read('config.ini')
-    file_path = config.get('app', 'file_path')
-    rpc_url = config.get('app', 'rpc_url')
-    proxy_url = config.get('app', 'proxy_url')
-    solver_provider = config.get('app', 'solver_provider')
-    client_key = config.get('app', 'client_key')
-    # 如果私钥文件,自动创建一个
-    if not os.path.exists(file_path):
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        with open(file_path, 'w') as file:
-            file.write('')
-    if mode_init_wallet:
-        # 预期领水的地址数
-        count = 1800
-        # 线程数
-        max_workers = 5
-        generate_wallet(count, rpc_url, proxy_url, solver_provider, client_key, file_path, max_workers)
-    else:
-        interaction_count = 0  # 初始化交互计数器
-        args = []
-        with open(file_path, 'r') as file:
-            for private_key in file:
-                private_key_str = private_key.strip()
-                private_key_item = private_key_str.split(",")
-                private_key_show = private_key_item[0]
-                args.append([private_key_show, rpc_url, proxy_url, solver_provider, client_key])
-                #break
-        random.shuffle(args)
-        args2 = []
-        index = 0
-        for item in args:
-            args2.append([index, item[0], item[1], item[2], item[3], item[4]])
-            index += 1
-        modeList = [only_claim, only_mint, claim_and_action]
-        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-            # 将任务提交给线程池
-            results = list(executor.map(modeList[0], args2))
+    file_path = './wallet/bera_private_keys.txt'
+    rpc_url = 'https://rpc.ankr.com/berachain_testnet'
+    proxy_url = ''
+    solver_provider = ''
+    client_key = ''
+
+    interaction_count = 0  # 初始化交互计数器
+    args = []
+    with open(file_path, 'r') as file:
+        for private_key in file:
+            private_key_str = private_key.strip()
+            private_key_item = private_key_str.split(",")
+            private_key_show = private_key_item[0]
+            args.append([private_key_show, rpc_url, proxy_url, solver_provider, client_key])
+    random.shuffle(args)
+    args2 = []
+    index = 0
+    for item in args:
+        index += 1
+        args2.append([index, item[0], item[1], item[2], item[3], item[4]])
+    claim_and_action(args2[0])
