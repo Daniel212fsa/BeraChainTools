c7724f1d31e71ee114cadbbac7601c2d7122bded
kukuxz007 <kukuxz007@gmail.com>
2024-02-19 09:52:01 +0800

    添加域名交互以及随机rpc

--- config/abi_config.py
+++ config/abi_config.py
@@ -3215,5 +3215,7 @@ domain_abi = [
         "outputs": [],
         "stateMutability": "payable",
         "type": "function"
-    }
+    },
+    {"type": "function", "name": "balanceOf", "stateMutability": "view",
+     "inputs": [{"name": "account", "type": "address"}], "outputs": [{"name": "", "type": "uint256"}]}
 ]
--- main.py
+++ main.py
@@ -17,15 +17,15 @@ from bera_tools import BeraChainTools
 # from utils.waters_utils import get_proxy
 import concurrent.futures
 from functools import partial
+import random
 
 
 def only_send20(arg):
     current_timestamp = time.time()
-    index, private_key, rpc_url, proxy_url, solver_provider, client_key, try_times = arg
+    index, private_key, rpc_url, proxy_url, solver_provider, client_key, try_times, rpc_list = arg
     try:
         account = Account.from_key(private_key)
         account_address = account.address
-        # logger.debug(f"第{index}次交互,地址:{account_address}")
         bera = BeraChainTools(private_key=private_key,
                               proxy_url=proxy_url,
                               client_key=client_key,
@@ -41,6 +41,13 @@ def only_send20(arg):
         logger.error(f'第{index}次交互,{account_address},领水失败,{e},耗时{time.time() - current_timestamp}')
 
 
+def get_rand_rpc(rpc_url0, rpc_list0):
+    if len(rpc_list0) > 0:
+        return random.choice(rpc_list0.split(","))
+    else:
+        return rpc_url0
+
+
 def only_create_domain_nft(arg):
     name = '创建域名'
     current_timestamp = time.time()
@@ -48,7 +55,7 @@ def only_create_domain_nft(arg):
     try:
         account = Account.from_key(private_key)
         account_address = account.address
-        # logger.debug(f"第{index}次交互,地址:{account_address}")
+        logger.debug(f'第{index}次交互,{account_address},{name},rpc:{rpc_url}')
         bera = BeraChainTools(private_key=private_key,
                               proxy_url=proxy_url,
                               client_key=client_key,
@@ -173,6 +180,29 @@ def claim_and_action(arg):
         logger.error(f'第{index}次交互,{account_address},{e}')
 
 
+def only_dex(arg):
+    current_timestamp = time.time()
+    index, private_key, rpc_url, proxy_url, solver_provider, client_key, try_times = arg
+    try:
+        account = Account.from_key(private_key)
+        account_address = account.address
+        logger.debug(f"第{index}次交互,地址:{account_address}")
+        bera = BeraChainTools(private_key=private_key,
+                              proxy_url=proxy_url,
+                              client_key=client_key,
+                              solver_provider=solver_provider,
+                              rpc_url=rpc_url)
+        balance = bera.get_balance()
+        if balance > 0:
+            bex_interacte(private_key, rpc_url, index, try_times)
+            honey_interacte(private_key, rpc_url, index, try_times)
+            logger.debug(f'第{index}次交互,{account_address},交互结束,耗时{time.time() - current_timestamp}')
+        else:
+            logger.error(f'第{index}次交互,{account_address},测试币不足,耗时{time.time() - current_timestamp}')
+    except Exception as e:
+        logger.error(f'第{index}次交互,{account_address},{e}')
+
+
 def only_action(arg):
     current_timestamp = time.time()
     index, private_key, rpc_url, proxy_url, solver_provider, client_key, try_times = arg
@@ -229,6 +259,7 @@ if __name__ == '__main__':
     proxy_url = get_app_item(app, 'proxy_url')
     solver_provider = get_app_item(app, 'solver_provider')
     client_key = get_app_item(app, 'client_key')
+    rpc_list = get_app_item(app, 'rpc_list')
     # print(file_path, rpc_url, proxy_url, solver_provider, client_key)
     # 如果私钥文件,自动创建一个
     if not os.path.exists(file_path):
@@ -240,7 +271,8 @@ if __name__ == '__main__':
         count = 1800
         # 线程数
         max_workers = 5
-        generate_wallet(count, rpc_url, proxy_url, solver_provider, client_key, file_path, max_workers)
+        generate_wallet(count, get_rand_rpc(rpc_url, rpc_list), proxy_url, solver_provider, client_key, file_path,
+                        max_workers)
     else:
         interaction_count = 0  # 初始化交互计数器
         args = []
@@ -250,16 +282,25 @@ if __name__ == '__main__':
                 private_key_str = private_key.strip()
                 private_key_item = private_key_str.split(",")
                 private_key_show = private_key_item[0]
-                args.append([private_key_show, rpc_url, proxy_url, solver_provider, client_key, try_times])
+                args.append([private_key_show, get_rand_rpc(rpc_url, rpc_list), proxy_url, solver_provider, client_key,
+                             try_times])
                 op += 1
-                break
+                # break
         random.shuffle(args)
         args2 = []
         index = 0
         for item in args:
             args2.append([index, item[0], item[1], item[2], item[3], item[4], item[5]])
             index += 1
-        modeList = [only_claim, only_mint, only_action, claim_and_action, only_send20, only_create_domain_nft]
-        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        modeList = [
+            only_claim,  # 0
+            only_mint,  # 1
+            only_action,  # 2
+            claim_and_action,  # 3
+            only_send20,  # 4
+            only_create_domain_nft,  # 5
+            only_dex,  # 6
+        ]
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
             # 将任务提交给线程池
             results = list(executor.map(modeList[5], args2))
